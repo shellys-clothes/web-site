309342017bd5b350b57e033d04e11f1006d70409
Kenner <kenner.ruizv@gmail.com>
2025-06-03 14:48:16 -0300

    atualizamos a apareciencia por uma padrão apenas no home , alteramos style.css, main.js , index.html

--- js/main.js
+++ js/main.js
@@ -1,2 +1,11 @@
 // js/main.js
 console.log("Shelly's Clothes inicializado ✨");
+
+  // Mobile menu toggle com animação
+  const toggleBtn = document.getElementById('menu-toggle');
+  const nav = document.getElementById('shelly-nav');
+
+  toggleBtn.addEventListener('click', () => {
+    toggleBtn.classList.toggle('active');
+    nav.classList.toggle('show');
+  });
